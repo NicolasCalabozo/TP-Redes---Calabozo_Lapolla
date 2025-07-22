89846f470a298ea7ef807a81d59161c970829ac8
martinfl95 <91093784+martinfl95@users.noreply.github.com>
2025-07-22 20:50:40 -0300

    Añadimos servicios
    Signed-off-by: martinfl95 <91093784+martinfl95@users.noreply.github.com>

---
+++ servicioServidor.py
@@ -0,0 +1,27 @@
+import json
+def getRawMovies():
+    with open('movies.json', 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+    return data
+
+def getMovieTitles() -> list[str]: 
+    data = getRawMovies()
+    titles = [movie['title'] for movie in data]
+    return titles
+
+def getFilteredMovies(filter_text: str) -> list[str]:
+    titles = getMovieTitles()
+    filtered_titles = []
+    for title in titles:
+        if filter_text.upper().strip() in title.upper():
+            filtered_titles.append(title)
+    return filtered_titles
+
+def getFilmography(name_filter: str):
+    data = getRawMovies()
+    filmography = []
+    for movie in data:
+        for cast in movie['cast']:
+            if name_filter.upper().strip() in cast.upper():
+                filmography.append(movie['title'])
+    return filmography
